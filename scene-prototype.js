3754786a519bc65db40d3daa09e8318b21ddf22f
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-01-29 20:56:37 -0500

    Remove obsolete comments and fix the dot amount in scene-prototype.js

--- scene-prototype.js
+++ scene-prototype.js
@@ -1,18 +1,3 @@
-// What is a scene? 
-
-// A scene contains... 
-// A scene should give 25000 positions on demand... and also interpolate with another scene...
-
-// When interpolating with another scene... 
-// it should activate the update() method of this other scene, to get the positions.
-// A scene should have an init() function that could be run before the update function
-// runs once... 
-// The purpose of an init() function is to deal with scenes that are chaotic
-// in nature and need a little bit of time to settle towards an attractor.
-// So... theoretically... the init() function could generate 25,000 fixed positions
-// as the initial state of the scene... 
-
-
 let Scene = function(name) {
     this.name = name;
     this.vertices = [];
@@ -48,5 +33,5 @@ Scene.prototype.display = function() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
     gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coord);
-    gl.drawArrays(gl.POINTS, 0, amountX * amountY);
+    gl.drawArrays(gl.POINTS, 0, 2500);
 };
