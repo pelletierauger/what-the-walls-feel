3393acd80b0fdc9660af6644d42ff74b2042ca9e
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-01-29 22:43:39 -0500

    Make the alt key play and pause the animation

--- sketch.js
+++ sketch.js
@@ -172,5 +172,35 @@ function keyPressed() {
         if (key == 'p' || key == 'P') {
             frameExport();
         }
+        // if (keyCode == "ESCAPE") {
+        //     if (looping) {
+        //         noLoop();
+        //         looping = false;
+        //     } else {
+        //         loop();
+        //         looping = true;
+        //     }
+        // }
     }
 }
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        // isEscape = (evt.key === "Escape" || evt.key === "Esc");
+        isEscape = (evt.key === "AltRight" || evt.key === "Alt");
+    } else {
+        // isEscape = (evt.keyCode === 27);
+        isEscape = (evt.keyCode === 18);
+    }
+    if (isEscape) {
+        if (looping) {
+            noLoop();
+            looping = false;
+        } else {
+            loop();
+            looping = true;
+        }
+    }
+};
