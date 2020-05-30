47d0cb52e517be06e332b4d25ddc44028c524517
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-05-29 23:05:04 -0400

    Write mistyProgram2 and apply it to bigTravelInABrokenLand

--- new-scenes.js
+++ new-scenes.js
@@ -1565,3 +1565,64 @@ bigTravelInABrokenLand.display = function(alpha) {
     gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.TRIANGLES, 0, numItems);
 };
+
+bigTravelInABrokenLand.display = function(alpha) {
+    // We start by drawing the whole image on the first texture, "texture"
+    let oneTextureProgram = getProgram("one-texture-program");
+    let mistyProgram2 = getProgram("misty-program-2");
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    // We bind the framebuffer...
+    bindFrameBuffer(texture, framebuf);
+    gl.viewport(0, 0, 1280, 720);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    if (drawCount >= 5) {
+        currentProgram = getProgram("blue-background");
+        gl.useProgram(currentProgram);
+        // gl.uniform1f(time, drawCount * 0.00125);
+        gl.uniform1f(time, 0.125 + drawCount * 0.00025);
+        drawBG(currentProgram);
+    }
+    currentProgram = getProgram("cyan-dots");
+    gl.useProgram(currentProgram);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, dotsVBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coord);
+    gl.drawArrays(gl.POINTS, 0, 2500);
+// 
+// 
+    // unbind the buffer and draw the resulting texture....
+// 
+    let vertices = new Float32Array([-1, 1, 1, 1, 1, -1, // Triangle 1
+        -1, 1, 1, -1, -1, -1 // Triangle 2
+    ]);
+    let itemSize = 2;
+    let numItems = vertices.length / itemSize;
+    // let vbuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+// 
+    currentProgram = mistyProgram2;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.viewport(0, 0, 1280, 720);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.useProgram(currentProgram);
+// 
+    currentProgram.aVertexPosition = gl.getAttribLocation(currentProgram, "a_position");
+    gl.enableVertexAttribArray(currentProgram.aVertexPosition);
+    gl.vertexAttribPointer(currentProgram.aVertexPosition, itemSize, gl.FLOAT, false, 0, 0);
+// 
+    textureLocation = gl.getUniformLocation(currentProgram, "u_texture");
+    gl.uniform1i(textureLocation, 0);
+    timeLocation = gl.getUniformLocation(currentProgram, "time");
+    gl.uniform1f(timeLocation, drawCount * 0.01);
+    alphaLocation = gl.getUniformLocation(currentProgram, "alpha");
+    gl.uniform1f(alphaLocation, alpha);
+// 
+    texcoordLocation = gl.getAttribLocation(currentProgram, "a_texcoord");
+    gl.enableVertexAttribArray(texcoordLocation);
+    gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, numItems);
+};
--- shaders.js
+++ shaders.js
@@ -1109,3 +1109,100 @@ void main() {
 `;
 foggyProgram.init();
 // redraw();
+
+let mistyProgram2 = new ShaderProgram("misty-program-2");
+
+mistyProgram2.vertText = `
+// beginGLSL
+attribute vec3 a_position;
+attribute vec2 a_texcoord;
+varying vec2 v_texcoord;
+void main() {
+    // Multiply the position by the matrix.
+    vec4 positionVec4 = vec4(a_position, 1.0);
+    // gl_Position = a_position;
+    positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+    gl_Position = positionVec4;
+    // Pass the texcoord to the fragment shader.
+    v_texcoord = a_texcoord;
+}
+//  endGLSL
+`;
+
+mistyProgram2.fragText = `
+// beginGLSL
+precision mediump float;
+// Passed in from the vertex shader.
+uniform float time;
+uniform float alpha;
+varying vec2 v_texcoord;
+// The texture.
+uniform sampler2D u_texture;
+float plot(vec2 s, float p) {
+  float largeur = abs(sin(time * 0.01)) * 0.1 + 0.1;
+  return smoothstep(p - largeur, p, s.y) - smoothstep(p, p + largeur, s.y);
+}
+float circ(float size, float vx, float vy, vec2 center) {
+//   float x = center.x * (1.0 + sin(time * 20.) * 0.5);
+//   float y = center.y * (1.0 + cos(time * 20.) * 0.5);
+  vec2 v = center - vec2(vx, vy);
+  float d = 1.0 / length(v * size);
+  return d;
+}
+float Circle(vec2 uv, vec2 p, float r, float blur) {
+    float d = length(uv - p); 
+    float c = smoothstep(r, r - blur, d); 
+    return c;
+}
+vec3 CircleRGB(vec2 uv, vec2 p, float r, float blur, vec3 col) {
+    float d = length(uv - p); 
+    float c = smoothstep(r, r - blur, d); 
+    return col * c;
+}
+vec3 InvCircleRGB(vec2 uv, vec2 p, float r, float blur, vec3 col) {
+    float d = length(p - uv); 
+    float c = smoothstep(r - blur, r, d); 
+    return col * c;
+}
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(time)));
+}
+void main() {
+    vec2 uv = vec2(gl_FragCoord.xy) / vec2(1600, 1600);
+    float t = time * 0.01;
+//     vec2 p2 = vec2(4.0, 0.2) * 0.1;
+//     vec3 col = InvCircleRGB(uv, p2, 0.5, 0.2, vec3(1.0, 1.0, 0.7));
+//     vec3 colb = InvCircleRGB(uv, p2, 0.5, 0.2, vec3(1.0, 0.0, 0.0));
+//     vec3 col2 = CircleRGB(uv, p2, 0.5, 0.4, vec3(1.0, 0.0, 0.0));
+//     float rando = rand(vec2(uv.x, uv.y));
+//     float r = 0.3;
+    vec2 p = vec2(1.0, -1.4);
+    float c = Circle(uv, p, 0.3, 0.1);
+    vec2 p2 = vec2(0.1 * sin(t * 100.), 0.1 * cos(t * 100.)) * 0.4;
+    vec2 p3 = vec2(0.1 * sin(t * 50.), 0.1 * cos(t * 50.)) * 0.4;
+    float siz = sin(t * 100.);
+    float siz2 = sin(t * 50.);
+    p2 = vec2(0.0, 0.0);
+    vec3 col = CircleRGB(uv, p2, 1.9, 0.2, vec3(4., 0.4, 0.0));
+    col.r *= sin(length(uv - p) * (6. + sin(1. * 20.) * 0.0001));
+    col.g *= sin(length(uv - p) * (6. + sin(1. * 20.) * 0.01));
+    vec2 newV = (uv + tan(uv.y * 3.)) - (p + sin(uv.x * 3.));
+    newV += cos(sin(uv * 20.));
+    col *= sin(atan(newV.x, newV.y));
+    col *= sin(sin(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + sin(t * 10.) * 100.);
+//     float rando = rand(uvf) * 0.075;
+// //     col.r -= rando * 1.;
+//      col.g *= 0.5;
+//     gl_FragColor = vec4((col- rando) * 1.0, 1.0);
+    gl_FragColor = texture2D(u_texture, v_texcoord);
+    gl_FragColor.rgb -= col * 0.75;
+//     gl_FragColor.rgb += col2 * 0.75;
+//     gl_FragColor.rgb += colb * 0.85;
+    gl_FragColor.a *= alpha;
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // gl_FragColor.r = gl_FragColor.r * 2.5;
+    // gl_FragColor.a = 0.5;
+}
+// endGLSL
+`;
+mistyProgram2.init();
