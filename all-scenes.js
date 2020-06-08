64476e7443c5b8e12fd981f43d774b107b61633a
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-06-07 21:43:23 -0400

    Some work on the storm sequence

--- all-scenes.js
+++ all-scenes.js
@@ -14,6 +14,9 @@ neutral.update = function(sum) {
             yy += Math.random() * 0.01;
             xx *= 0.95;
             this.vertices.push(xx * 0.075 - 1.2115, (yy + -10) * 0.07 - 1.32); // good
+//             if (x == amountX && y == 0) {
+//                 logJavaScriptConsole(xx);
+//             }
         }
     }
 };
@@ -1458,31 +1461,20 @@ middleOfTheMuscularDistanceFieldB.update = function(sum) {
     let amountX = 50;
     let amountY = 50;
     let t = (drawCount - sum + 120800) * 0.005;
-    let a = 0.005 * sin(t * 0.05);
-    let i = 0;
     for (let x = amountX; x > 0; x -= 1) {
-        //     for (let x = 0; x < amountX; x += 1) {
         for (let y = 0; y < amountY; y += 1) {
-            let ox = x - 25;
-            let oy = y - 46;
+            let ox = x - 15;
+            let oy = y - 46 + 8;
             let dx = abs(cos(x) * 1) * 0.25;
             let dy = abs(sin(y) * 1) * 0.25;
             let xx = x - pow(cos(cos(dx + oy * ox * 0.01) + t * 1.5), -1) * 1.25;
             let yy = y + pow(sin(sin(dx + oy * ox * 0.01) + t * 1.5), -1) * 0;
-            //             xx += map(cos(x * t), -1, 1, 0.5, 0.4) * 2;
-            //             yy += map(sin(y * t), -1, 1, 0.5, 0.4) * 2;
-            if (i == 0) {
-                oriX = xx;
-            }
-            //             let ranX = Math.random() * 0.025 * 0.5;
-            //             let ranY = Math.random() * 0.025 * 0.5;
-            //             this.vertices.push((xx - 0 + ranX) * 0.05 * 1.0 - 1.2, (yy + ranY) * 0.05 * 1.0 - 1.3);
-            //             this.vertices.push((xx - 0) * 0.075 * 1.0 - 1.2, (yy + -10) * 0.07 * 1.0 - 1.3);
-            xx += Math.random() * 0.01;
-            yy += Math.random() * 0.01;
             xx *= 0.95;
+            xx -= 1.2; // Adjustement to make it fit with the neutral scene.
             this.vertices.push(xx * 0.075 - 1.2115, (yy + -10) * 0.07 - 1.32); // good
-            i++;
+//             if (x == amountX && y == 0) {
+//                 logJavaScriptConsole(xx);
+//             }
         }
     }
 };
